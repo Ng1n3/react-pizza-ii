3379a7d38d3008d532d97433bf23d1ce4ffe730b
ng1n3 <code.with.muyiwa@gmail.com>
2024-03-24 09:07:46 +0100

    reusing styles using react components

---
+++ src/ui/LinkButton.jsx
@@ -0,0 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom"
+
+function LinkButton({children, to}) {
+  const navigate = useNavigate();
+  const className = 'text-sm text-blue-500 hover:text-blue-600'
+
+  if(to === '-1') return <button className={className} onClick={() => navigate(-1)}>&larr; Go back</button>
+  return (
+    <Link to={to} className={className}>{children}
+    </Link>
+  )
+}
+
+export default LinkButton;
