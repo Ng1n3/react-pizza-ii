82343d6a0c45d59bf336f1b232dd6d5a5dd553ca
ng1n3 <code.with.muyiwa@gmail.com>
2024-03-24 12:57:44 +0100

    custom font family, size, height

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: 'Roboto Mono, monospace'
+    },
+      // fontSize: {
+      //   huge: ['80rem', {lineHeight: '1'}],
+      // },
+      height: {
+        screen: '100dvh',
+      },
     extend: {},
   },
   plugins: [],
